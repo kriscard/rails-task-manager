eacab9fe84ad7650f660e2225f690607443743c9
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-02-13 16:52:35 +0100

    exercise done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
+
+  def show
+  end
+
+  def new
+    @task = Task.new()
+  end
+
+  def create
+   @task_create = Task.new(task_params)
+   @task_create.save
+   redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+  @task = Task.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+
+  post   "tasks",          to: "tasks#create"
+  # create
+  get    "tasks/new",      to: "tasks#new", as: :new_task
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  # show
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # edit
+  patch  "tasks/:id",      to: "tasks#update"
+
+  #delete
+  delete "tasks/:id",      to: "tasks#destroy", as: :delete
+
+  # resources :tasks
 end
